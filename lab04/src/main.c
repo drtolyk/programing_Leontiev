c1e3e5bbc43b3b3fa53e129d489ccb8036f0ac5a
drtolyk <dr.tolyklol@gmail.com>
2022-10-27 12:47:21 +0300

    labb04

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -3,19 +3,19 @@ int main () {
 char *text= "v";
         const int radius = 13;
         const float pii = 3.14;
-        printf("Визначення площi довжини та об`эму за заданим радiусом r=%d\n, для цього введiть s, l або v", radius);
+        printf("Визначення площi довжини та об`эму за заданим радiусом r=%d\n для цього введiть s, l або v", radius);
         if (text == "s" )
 {
         float plosha = pii * radius * radius;
-        printf("S=%f", plosha); }
+        printf("\nS=%f", plosha); }
         else if (text == "l" )
 {
         float dlina = 2 * pii * radius;
-        printf("L=%f", dlina); }
+        printf("\nL=%f", dlina); }
         else if (text == "v" )
 {
         float obem = 4.0 / 3.0 * pii * (radius * radius * radius);
-        printf("V=%f ", obem); }
+        printf("\nV=%f ", obem); }
         else printf ("Помилка вводу");
 	return 0;
 }
