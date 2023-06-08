ac53e92c25e1f16448e9f77bb834a775a845ef17
drtolyk <dr.tolyklol@gmail.com>
2023-06-08 23:32:47 +0300

    anatoly

---
+++ lab10/module-library/src/lib.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+	*Розрахунок НСД
+	@param num1 Перше число
+	@param num2 Друге число
+	@param find_nsd Розрахунок НСД
+
+*/
+int find_nsd(int num1,int num2);
